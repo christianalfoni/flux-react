bdc9f4966dbabb7d365087b463252d3efe557634
Christian Alfoni <christianalfoni@gmail.com>
2014-08-20 19:44:20 +0200

    Fixed context issue

--- app/ExtendedComponent.js
+++ app/ExtendedComponent.js
@@ -13,7 +13,7 @@ function ExtendedComponent (dispatcher, props) {
 			var doUpdate = true;
 			this.storesDidUpdate = function () {
 				if (doUpdate) {
-					props.storesDidUpdate.call(store);
+					props.storesDidUpdate.call(this);
 					doUpdate = false;
 					process.nextTick(function () {
 						doUpdate = true;
